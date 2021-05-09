3c24114ab46f35e5f9da3ecf54c64f854630fbb3
ekographs <nwaforlorekoate@hotmail.com>
2021-05-09 04:15:55 +0400

    update on tracking

--- mail/contact.js
+++ mail/contact.js
@@ -5,50 +5,17 @@ $(function () {
         submitError: function ($form, event, errors) {
         },
         submitSuccess: function ($form, event) {
-            event.preventDefault();
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var subject = $("input#subject").val();
-            var message = $("textarea#message").val();
-
-            $this = $("#sendMessageButton");
-            $this.prop("disabled", true);
-
-            $.ajax({
-                url: "contact.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message
-                },
-                cache: false,
-                success: function () {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
+            var trackinginput = $("#tracking").val();
+            if (trackinginput !="156XE57CHI72GLD4"){
+                event.preventDefault();
+                $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                     .append("</button>");
+                    $('#success > .alert-danger').append($("<strong>").text(" Tracking number not in our system"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
+            }        
                 },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false);
-                    }, 1000);
-                }
-            });
-        },
         filter: function () {
             return $(this).is(":visible");
         },
